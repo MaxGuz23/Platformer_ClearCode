0439422245334eb2e08ae230f41c596e52102c01
Max Guzman <maxguzman.23@hotmail.com>
2023-01-01 19:21:00 -0600

    Update level

---
+++ settings.py
@@ -0,0 +1,19 @@
+level_map = [
+    '                           ',
+    '                           ',
+    '                           ',
+    ' XX    X              XX   ',
+    ' XX                        ',
+    ' XXXX         XX         XX',
+    ' XXXX       XX             ',
+    ' XX    X  XXXX             ',
+    '       X  XXXX             ',
+    '    XXXX  XXXXXX  XX  XXXX ',
+    'XXXXXXXX  XXXXXX  XX  XXXX ',
+]
+
+TILE_SIZE = 64
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = TILE_SIZE * len(level_map)
+
+print(SCREEN_HEIGHT)
