0da8d6d8bee97acf5be97b84c133eed11494fc0c
Max Guzman <maxguzman.23@hotmail.com>
2023-01-05 14:57:23 -0600

    adding player horizontal movement

--- level.py
+++ level.py
@@ -1,6 +1,7 @@
 import pygame
 from tiles import Tile
-from settings import TILE_SIZE
+from settings import TILE_SIZE, SCREEN_WIDTH
+from player import Player
 class Level:
     def __init__(self,level_data,surface):
         self.display_surface = surface
@@ -9,15 +10,39 @@ class Level:
     
     def setup_level(self,layout):
         self.tiles = pygame.sprite.Group()
+        self.player = pygame.sprite.GroupSingle()
+
         for row_index, row in enumerate(layout):
             for col_index, cell in enumerate(row):
+                x = col_index * TILE_SIZE
+                y = row_index * TILE_SIZE
                 if cell == 'X':
-                    x = col_index * TILE_SIZE
-                    y = row_index * TILE_SIZE
                     tile = Tile((x,y), TILE_SIZE)
                     self.tiles.add(tile)
+                if cell == 'P':
+                    player_sprite = Player((x,y))
+                    self.player.add(player_sprite)
 
+    def scroll_x(self):
+        player = self.player.sprite
+        player_x = player.rect.centerx
+        direction_x = player.direction.x
+
+        if player_x < SCREEN_WIDTH/4 and direction_x < 0:
+            self.world_shift = 8
+            player.speed = 0
+
+        elif player_x > SCREEN_WIDTH - (SCREEN_WIDTH/4) and direction_x > 0:
+            self.world_shift = -8
+            player.speed = 0
+        else:
+            self.world_shift = 0
+            player.speed = 8
 
     def run(self):
         self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
+
+        self.player.update()
+        self.player.draw(self.display_surface)
+        self.scroll_x()
